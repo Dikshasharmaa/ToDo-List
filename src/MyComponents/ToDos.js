0efbdd5bdb6a9177349c4278ccb0f56602579cad
Dikshasharmaa <diksha66sharma78@gmail.com>
2024-04-27 01:11:15 -0400

    Add beautiful footer

--- src/MyComponents/ToDos.js
+++ src/MyComponents/ToDos.js
@@ -3,12 +3,14 @@ export const ToDos = ({todos,onDelete}) => {
   return (
     <div className="container">
         <h3 className='text-center'>ToDo List</h3>
-        {todos.map(item =>(<div className='todo' key={item.sno}>
+        {todos.length ===0? "No ToDos to display":   
+        todos.map(item =>(<div className='todo' key={item.sno}>
           <h2>{item.title}</h2>
           <p>{item.description}</p>
           <button className='btn btn-sm btn-danger'onClick ={()=>{onDelete(item)}}>Delete </button>
         </div>))
         }
+      
 
         </div>
   );
