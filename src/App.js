fc06abcc90859e7bdfa75a2a70997d2f44c9f619
Dikshasharmaa <diksha66sharma78@gmail.com>
2024-04-27 15:32:26 -0400

    Made a to-do list App using React

--- src/App.js
+++ src/App.js
@@ -1,57 +1,57 @@
-import './App.css';
-import Header from './MyComponents/Header';
-import { ToDos}  from './MyComponents/ToDos';
-import  { Footer}  from './MyComponents/Footer';
-import { AddTodo } from './MyComponents/AddTodo';
-import { useState, useEffect } from 'react';
+import "./App.css";
+import Header from "./MyComponents/Header";
+import { ToDos } from "./MyComponents/ToDos";
+import { Footer } from "./MyComponents/Footer";
+import { AddTodo } from "./MyComponents/AddTodo";
+import React, { useState, useEffect } from "react";
 
 function App() {
   let initTodo;
-  if(localStorage.getItem("todos")===null){
-    initTodo =[];
+  if (localStorage.getItem("todos") === null) {
+    initTodo = [];
+  } else {
+    initTodo = JSON.parse(localStorage.getItem("todos"));
   }
-  else{
-      initTodo = JSON.parse(localStorage.getItem("todos"));
-    }
-  const onDelete = (todo)=>{
-    console.log("I am deleting this task",todo);
+  const onDelete = (todo) => {
+    console.log("I am deleting this task", todo);
 
-    settodos(todos.filter((e)=>{
-      return e!==todo;
-    }));
-    localStorage.setItem("todos",JSON.stringify(todos));
-  }
-  const addTodo = (title,description)=>{
-    console.log("I am adding this todo",title,description);
+    settodos(
+      todos.filter((e) => {
+        return e !== todo;
+      })
+    );
+    localStorage.setItem("todos", JSON.stringify(todos));
+  };
+  const addTodo = (title, description) => {
+    console.log("I am adding this todo", title, description);
     let sno;
-    if(todos.length ===0){
+    if (todos.length === 0) {
       sno = 1;
+    } else {
+      sno = todos[todos.length - 1].sno + 1;
     }
-    else{
-        sno = todos[todos.length-1].sno +1;
-    }
-    const myToDo ={
-      sno:sno,
-      title:title,
-      description:description,
-    }
-    settodos([...todos,myToDo]);
+    const myToDo = {
+      sno: sno,
+      title: title,
+      description: description,
+    };
+    settodos([...todos, myToDo]);
     console.log(myToDo);
-  }
-  const[todos,settodos] = useState(initTodo);
-    useEffect(()=>{
-      localStorage.setItem("todos",JSON.stringify(todos));
-      },[todos])
+  };
+  const [todos, settodos] = useState(initTodo);
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   // const listItems = todos.map((todo) => {
   //   <li>{todo}</li>
   // })
   return (
     <>
-    <Header title= "MyToDoList" searchBar={true}/>
-    <AddTodo addTodo = {addTodo}></AddTodo>
-    <ToDos todos = {todos} onDelete={onDelete}></ToDos>  
-    <Footer></Footer>
+      <Header title="MyToDoList" searchBar={true} />
+      <AddTodo addTodo={addTodo}></AddTodo>
+      <ToDos todos={todos} onDelete={onDelete}></ToDos>
+      <Footer />
     </>
   );
 }
--- src/MyComponents/todoItems.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const todoItems = ({todo}) => {
-  return (
-    <h3>Hello</h3>
-  )
-}
